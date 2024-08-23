6a29ed70bca87f2b25e26862589ec10853afc4a3
AftabM23 <aftaba.mohammed23@gmail.com>
2024-08-22 20:32:37 -0400

    Added sample from with absolute position

---
+++ src/Components/Form.jsx
@@ -0,0 +1,10 @@
+import styles from "./Form.module.css";
+function Form() {
+  return (
+    <div className={styles.form}>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default Form;
--- src/Pages/InputSection.jsx
+++ src/Pages/InputSection.jsx
@@ -1,10 +1,19 @@
 import InputField from "../Components/InputField";
 import styles from "./InputSection.module.css";
 import Notes from "../Components/Notes";
+import Button from "../Components/Button";
+import Form from "../Components/Form";
+import { useState } from "react";
 function InputSection() {
+  const [showForm, setShowForm] = useState(false);
+  const handleOpenForm = () => {
+    setShowForm((show) => !show);
+  };
   return (
     <div className={styles.inputSection}>
       <InputField />
+      <Button onClick={handleOpenForm}> Open form</Button>
+      {showForm && <Form />}
       <Notes />
     </div>
   );
